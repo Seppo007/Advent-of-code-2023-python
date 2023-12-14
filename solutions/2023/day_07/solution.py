8dad22991bd545b9b82c8706f149678411d064fa
Sebastian Schalow <schalowseb@gmail.com>
2023-12-14 20:29:58 +0100

    Solve day7 part2 by counting jacks as jokers

--- solutions/2023/day_07/solution.py
+++ solutions/2023/day_07/solution.py
@@ -6,24 +6,41 @@ from ...base import TextSolution, answer
 from functools import cmp_to_key
 
 card_order_string = '23456789TJQKA'
+card_order_string_jack = 'J23456789TQKA'
+
+part2 = False
 
 
 def cards_comparator(hand1, hand2):
     cards1: str = hand1[0]
     cards2: str = hand2[0]
 
-    for index, _ in enumerate(cards1):
-        if card_order_string.index(cards1[index]) < card_order_string.index(cards2[index]):
-            return -1
-        elif card_order_string.index(cards1[index]) > card_order_string.index(cards2[index]):
-            return 1
-        else:
-            continue
-
-    return 0
+    if part2 is False:
+        for index, _ in enumerate(cards1):
+            if card_order_string.index(cards1[index]) < card_order_string.index(cards2[index]):
+                return -1
+            elif card_order_string.index(cards1[index]) > card_order_string.index(cards2[index]):
+                return 1
+            else:
+                continue
+    else:
+        for index, _ in enumerate(cards1):
+            if card_order_string_jack.index(cards1[index]) < card_order_string_jack.index(cards2[index]):
+                return -1
+            elif card_order_string_jack.index(cards1[index]) > card_order_string_jack.index(cards2[index]):
+                return 1
+            else:
+                continue
 
 
 def is_full_house(counts):
+    if part2 is True and counts.get('J') is not None:
+        jack_count = counts.pop('J') if counts.get('J') is not None else 0
+        if list(counts.values()).count(2) == 2 and jack_count == 1:
+            return True
+    else:
+        counts = counts.values()
+
     return list(counts).count(2) == 1 and list(counts).count(3) == 1
 
 
@@ -37,7 +54,7 @@ def get_type_for_(biggest_pair, counts):
     elif biggest_pair == 4:
         return "four_of_a_kind"
     elif biggest_pair == 3:
-        if is_full_house(counts.values()):
+        if is_full_house(counts):
             return "full_house"
         else:
             return "three_of_a_kind"
@@ -56,6 +73,16 @@ def get_type(hand):
         counts[card] = cards.count(card)
 
     biggest_pair = max(counts.values())
+    jack_count = counts.get('J')
+
+    if part2 is True and jack_count is not None:
+        old_counts = counts.copy()
+        counts.pop('J')
+        if jack_count == 5:
+            biggest_pair = 5
+        else:
+            biggest_pair = max(counts.values()) + jack_count
+        counts = old_counts
 
     return get_type_for_(biggest_pair, counts)
 
@@ -97,7 +124,6 @@ class Solution(TextSolution):
 
     # @answer(1234)
     def part_1(self) -> int:
-
         hands_with_bids = []
         for entry in self.input.split('\n'):
             entries = entry.split(' ')
@@ -123,7 +149,31 @@ class Solution(TextSolution):
 
     # @answer(1234)
     def part_2(self) -> int:
-        pass
+        global part2
+        part2 = True
+
+        hands_with_bids = []
+        for entry in self.input.split('\n'):
+            entries = entry.split(' ')
+            hands_with_bids.append((entries[0], int(entries[1])))
+
+        kinds: dict = {
+            'five_of_a_kind': [],
+            'four_of_a_kind': [],
+            'full_house': [],
+            'three_of_a_kind': [],
+            'two_pair': [],
+            'one_pair': [],
+            'high_card': []
+        }
+
+        for hand in hands_with_bids:
+            hand_type = get_type(hand)
+            newEntry = kinds.get(hand_type)
+            newEntry.append(hand)
+            kinds[hand_type] = newEntry
+
+        return attach_rank_to(kinds)
 
     # @answer((1234, 4567))
     # def solve(self) -> tuple[int, int]:
